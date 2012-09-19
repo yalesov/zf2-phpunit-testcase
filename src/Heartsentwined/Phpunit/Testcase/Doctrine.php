340e16ad49b65b69fdd83f4a5cdf23d438955531
heartsentwined <heartsentwined@cogito-lab.com>
2012-09-19 15:20:17 +0800

    refactoring: use setters instead of setUp() args

--- src/Heartsentwined/Phpunit/Testcase/Doctrine.php
+++ src/Heartsentwined/Phpunit/Testcase/Doctrine.php
@@ -3,27 +3,53 @@ namespace Heartsentwined\Phpunit\Testcase;
 
 use Doctrine\DBAL\Schema\SchemaException;
 use Doctrine\ORM\Tools\SchemaTool;
+use Heartsentwined\ArgValidator\ArgValidator;
 use Heartsentwined\FileSystemManager\FileSystemManager;
 
+/**
+ * Doctrine ORM setup: set EntityManager and proxy tmp dir
+ *
+ * @author heartsentwined <heartsentwined@cogito-lab.com>
+ * @license GPL http://opensource.org/licenses/gpl-license.php
+ */
 class Doctrine extends Zf
 {
     /**
-     * sets
-     * - $this->em      Doctrine\ORM\EntityManager
-     * - $this->tmpDir  (path to tmp dir)
+     * ServiceManager alias of EntityManager
      *
-     * @param string $bootstrap path to bootstrap file
-     *      bootstrap file should return Zend\Mvc\Application
-     * @param string $em ServiceManager alias of EntityManager
-     * @param string $tmpDir path to tmp dir
+     * @var string
+     */
+    protected $emAlias;
+    protected function setEmAlias($emAlias)
+    {
+        ArgValidator::assert($emAlias, array('string', 'min' => 1));
+        $this->emAlias = $emAlias;
+        return $this;
+    }
+
+    /**
+     * path to tmp dir, possibly for storing proxies
+     *
+     * @var string
+     */
+    protected $tmpDir;
+    protected function setTmpDir($tmpDir)
+    {
+        ArgValidator::assert($tmpDir, array('string', 'min' => 1));
+        $this->tmpDir = $tmpDir;
+        return $this;
+    }
+
+    /**
+     * setup EntityManager and tmp dir
+     *
      * @return void
      */
-    public function setUp($bootstrap, $em, $tmpDir)
+    public function setUp()
     {
-        parent::setup($bootstrap);
-        $this->em = $sm->get($em);
-        $this->tmpDir = $tmpDir;
+        parent::setup();
 
+        $this->em = $this->sm->get($this->emAlias);
         if (!is_dir($this->tmpDir)) mkdir($this->tmpDir);
 
         $metadatas = $this->em->getMetadataFactory()->getAllMetadata();
@@ -37,6 +63,11 @@ class Doctrine extends Zf
         }
     }
 
+    /**
+     * remove tmp dir
+     *
+     * @return void
+     */
     public function tearDown()
     {
         FileSystemManager::rrmdir($this->tmpDir);
--- src/Heartsentwined/Phpunit/Testcase/Zf.php
+++ src/Heartsentwined/Phpunit/Testcase/Zf.php
@@ -1,8 +1,10 @@
 <?php
 namespace Heartsentwined\Phpunit\Testcase;
 
+use Heartsentwined\ArgValidator\ArgValidator;
+
 /**
- * Zf application + service manager setup
+ * basic ZF application boostrap: setup application + service manager
  *
  * @author heartsentwined <heartsentwined@cogito-lab.com>
  * @license GPL http://opensource.org/licenses/gpl-license.php
@@ -10,17 +12,42 @@ namespace Heartsentwined\Phpunit\Testcase;
 class Zf extends \PHPUnit_Framework_TestCase
 {
     /**
-     * sets
+     * path to bootstrap file, which should return Zend\Mvc\Application
+     *
+     * @var string
+     */
+    protected $bootstrap;
+    protected function setBootstrap($bootstrap)
+    {
+        ArgValidator::assert($bootstrap, array('string', 'min' => 1));
+        $this->bootstrap = $bootstrap;
+        return $this;
+    }
+
+    /**
+     * ZF application instance
+     *
+     * @var Zend\Mvc\Application
+     */
+    protected $application;
+
+    /**
+     * ZF service manager instance
+     *
+     * @var Zend\ServiceManager\ServiceManager
+     */
+    protected $sm;
+
+    /**
+     * setup ZF application and service manager
      * - $this->application Zend\Mvc\Application
      * - $this->sm          Zend\ServiceManager\ServiceManager
      *
-     * @param string $bootstrap path to bootstrap file
-     *      bootstrap file should return Zend\Mvc\Application
      * @return void
      */
-    public function setUp($bootstrap)
+    public function setUp()
     {
-        $application = require $bootstrap;
+        $application = require $this->bootstrap;
         $this->application = $application;
         $this->sm = $application->getServiceManager();
     }
